8f1d56856c0dea20b80651234e9efe999e2bb3e4
tmaximini <tmaximini@gmail.com>
2014-01-16 15:47:26 +0100

    readme, responsive

--- app/scripts/pie.js
+++ app/scripts/pie.js
@@ -3,9 +3,11 @@ function makePie () {
 
   $(".svg-chart").empty();
 
-  var width = $(document).width() - 50,
-      height = $(document).height() < 500 ? $(document).height() : 500,
+  var width = $(window).width() - 50,
+      height = $(window).height() < 500 ? $(window).height() : 500,
       radius = Math.min(width, height) / 2;
+
+
 
   var color = d3.scale.ordinal()
       .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
