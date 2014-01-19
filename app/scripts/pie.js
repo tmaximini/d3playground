60033afc07590279bf6400d71f70235bae374715
tmaximini <tmaximini@gmail.com>
2014-01-19 17:37:14 +0100

    group spots

--- app/scripts/pie.js
+++ app/scripts/pie.js
@@ -6,7 +6,7 @@ function getRandomInt (min, max) { return Math.floor(Math.random() * (max - min 
 
 function makePie () {
 
-  $(".svg-chart").empty();
+  $('.svg-chart').empty();
 
   width = $(window).width() - 50;
   height = $(window).height() < 500 ? $(window).height() : 500;
@@ -15,41 +15,58 @@ function makePie () {
 
 
   var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+      .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
 
-  var arc = d3.svg.arc()
-      .outerRadius(radius - 10)
+  var outerArc = d3.svg.arc()
+      .outerRadius(radius)
+      .innerRadius(radius / 2);
+
+  var innerArc = d3.svg.arc()
+      .outerRadius(radius / 2)
       .innerRadius(0);
 
   var pie = d3.layout.pie()
       .sort(null)
       .value(function(d) { return d.value; });
 
-  var svg = d3.select(".svg-chart")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var svg = d3.select('.svg-chart')
+      .attr('width', width)
+      .attr('height', height)
+    .append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-  d3.json("data.json", function(error, data) {
+  d3.json('data.json', function(error, data) {
 
     data.forEach(function(d) {
       d.value = 100 / data.length;
     });
 
-    var g = svg.selectAll(".arc")
+
+
+    var g = svg.selectAll('.arc')
         .data(pie(data))
-      .enter().append("g")
-        .attr("class", "arc");
+      .enter().append('g')
+        .attr('class', 'arc');
 
-    g.append("path")
-        .attr("d", arc)
-        .style("fill", function(d) { return color(d.data.name); });
+    var inner = svg.selectAll('.inner-arc')
+        .data(pie(data))
+      .enter().append('g')
+        .attr('class', 'inner-arc');
 
-    g.append("text")
-        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-        .attr("dy", ".35em")
-        .style("text-anchor", "middle")
+
+    g.append('path')
+        .attr('d', outerArc)
+        .style('fill', function(d) { return color(d.data.name); });
+
+    inner.append('path')
+        .attr('d', innerArc)
+        .style('fill', '#ccc')
+        .style('stroke', '#fff');
+
+    g.append('text')
+        .attr('transform', function(d) { return 'translate(' + outerArc.centroid(d) + ')'; })
+        .attr('dy', '.35em')
+        .style('text-anchor', 'middle')
         .text(function(d) { return d.data.name; });
 
   });
@@ -57,14 +74,23 @@ function makePie () {
 
 
 function drawCircles () {
-  circleRadii = [5, 8, 10];
-  var circles = d3.select(".svg-chart").selectAll('circle')
+  console.log('radius is:' + radius);
+  circleRadii = [5, 8, 10, 8, 12, 5, 17];
+  var circles = d3.select('.svg-chart')
+                .append('g')
+                .attr('class', 'spots')
+                .selectAll('circle')
                 .data(circleRadii)
-              .enter().append("circle")
-                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-                .attr("cx", function() { return getRandomInt(-radius, radius); } )
-                .attr("cy", function() { return getRandomInt(-radius, radius); } )
-                .attr("r", function (d) { return d; });
+              .enter().append('g')
+                .attr('class', 'symptomSpot')
+                .append('circle')
+                .on('click', function(d,i) { console.log('Hello world', d, i); })
+                .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+                .attr('cx', function() { return getRandomInt(-radius / 2, radius / 2); } )
+                .attr('cy', function() { return getRandomInt(-radius / 2, radius / 2); } )
+                .attr('r', function (d) { return d; });
+
+
 
 }
 
